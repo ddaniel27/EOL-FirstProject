4c8bbab929161867270017fb2e9bc8397ec6f48c
Daniel Dorado <danieldorado27@gmail.com>
2021-12-23 18:29:39 -0500

    Final endpoint implementation with IMG in backend, needs a merge with email enhanced

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,8 @@
-const {body, validationResult } = require("express-validator");
+const {body, validationResult} = require("express-validator");
 
 // Controladores
 const { sendEmail } = require("../controllers/sendEmail.controller");
-const {addNewContactInfo, createConnection, addOrderID} = require("../controllers/sqlQueries.controller")
+const {addNewContactInfo, createConnection, addOrderID, getEmailByToken} = require("../controllers/sqlQueries.controller")
 const {getNewToken, validateToken} = require("../controllers/uuid.controller")
 const myConnection = createConnection()
 module.exports = (router) => {
@@ -10,16 +10,16 @@ module.exports = (router) => {
   router.post(
     "/sendinfo",
     [
-      body("email").isEmail(),
-      body("country").isAlpha('en-US', {ignore: /[\xE0-\xFF' ']/g}),
-      body("name").isAlpha('en-US', {ignore: /[\xE0-\xFF' ']/g}),
-      body("lastname").isAlpha('en-US', {ignore: /[\xE0-\xFF' ']/g}),
-      body("address").isAlphanumeric('en-US', {ignore: ' -'}),
-      body("wallet").isAlphanumeric(),
-      body("city").isAlpha('en-US', {ignore: /[\xE0-\xFF' ']/g}),
-      body("province").isAlpha('en-US', {ignore: /[\xE0-\xFF' ']/g}),
-      body("zipcode").isInt(),
-      body("phone").isInt(),
+      body("email").isEmail().not().isEmpty(),
+      body("country").isAlpha('en-US', {ignore: /[\xE0-\xFF' ']/g}).not().isEmpty(),
+      body("name").isAlpha('en-US', {ignore: /[\xE0-\xFF' ']/g}).not().isEmpty(),
+      body("lastname").isAlpha('en-US', {ignore: /[\xE0-\xFF' ']/g}).not().isEmpty(),
+      body("address").isAlphanumeric('en-US', {ignore: ' -'}).not().isEmpty(),
+      body("wallet").isAlphanumeric().not().isEmpty(),
+      body("city").isAlpha('en-US', {ignore: /[\xE0-\xFF' ']/g}).not().isEmpty(),
+      body("province").isAlpha('en-US', {ignore: /[\xE0-\xFF' ']/g}).not().isEmpty(),
+      body("zipcode").isInt().not().isEmpty(),
+      body("phone").isInt().not().isEmpty()
     ],
     async (req,res) => {
         let uploadPath;
@@ -28,6 +28,11 @@ module.exports = (router) => {
           res.status(400).json({errors:errors})
           return
         }
+        if(!req.files){
+          res.status(400).json({errors:[{msg:"No file uploaded"}]})
+          return
+        }
+        const imgDoc = req.files.kycDocument
         const newObj = {
             email: req.body.email.toLowerCase(),
             country: req.body.country.toLowerCase(),
@@ -39,13 +44,15 @@ module.exports = (router) => {
             province: req.body.province.toLowerCase(),
             zipcode: req.body.zipcode,
             phone: req.body.phone,
-            kycDocument: req.files.kycDocument
+            kycDocument: imgDoc.name
           }
-        uploadPath='./imageFolder/' + newObj.email + newObj.kycDocument.name
-        newObj.kycDocument.mv(uploadPath,function (err){
+        uploadPath='./imageFolder/' + newObj.email + newObj.kycDocument
+        imgDoc.mv(uploadPath,function (err){
           if(err) return res.status(500).send(err)
         })
         res.send('File upload')
+
+        newObj.kycDocument = newObj.email + newObj.kycDocument
         
         const sendEmailObj ={
           token: getNewToken(),
@@ -63,8 +70,8 @@ module.exports = (router) => {
   router.post(
     "/paypaltransaction",
     [
-      body("token").isAlphanumeric('en-US', {ignore: ' -'}),
-      body("orderID").isAlphanumeric('en-US', {ignore: ' -'})
+      body("token").isAlphanumeric('en-US', {ignore: ' -'}).not().isEmpty(),
+      body("orderID").isAlphanumeric('en-US', {ignore: ' -'}).not().isEmpty()
     ],
     async (req,res) => {
         const errors = validationResult(req).errors
@@ -82,6 +89,10 @@ module.exports = (router) => {
           }
           await addOrderID(myConnection, orderID, token)
           const email = await getEmailByToken(myConnection, token)
+          if(!email){
+            res.status(400).json({errors:["Invalid token"]})
+            return
+          }
           await sendEmail({email: email, token: orderID, isAuth:false},res)
         }catch(err){
           console.log(err)
