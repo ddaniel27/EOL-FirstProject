14a125feb6dd51ba990b5d2399cb1176acc95b8c
jmcc153 <jorgecasti@unicauca.edu.co>
2021-12-23 10:40:25 -0500

    Implementación de normalización del formulario

--- routes/router.js
+++ routes/router.js
@@ -4,8 +4,21 @@ const { check } = require("express-validator");
 const { sendEmail } = require("../controllers/sendEmail.controller");
 
 module.exports = (router) => {
-
   //Mailing
-  router.post("/sendVerificateEmail", sendEmail);
-
+  router.post(
+    "/sendVerificateEmail",
+    [
+      check("email").normalizeEmail().isEmail(),
+      check("country").isAlpha(),
+      check("name").isAlpha(),
+      check("lastname").isAlpha(),
+      check("address").isAlphanumeric(),
+      check("wallet").isBTCAddress(),
+      check("city").isAlpha(),
+      check("province").isAlpha(),
+      check("zipcode").isInt(),
+      check("phone").isInt(),
+    ],
+    sendEmail
+  );
 };
