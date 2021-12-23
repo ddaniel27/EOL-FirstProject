5ac51ceb3993a673ce20d6ab7fc409cb23888eb7
jmcc153 <jorgecasti@unicauca.edu.co>
2021-12-23 12:04:07 -0500

    implementación endpoint sendinfo

--- routes/router.js
+++ routes/router.js
@@ -2,23 +2,28 @@ const { check } = require("express-validator");
 
 // Controladores
 const { sendEmail } = require("../controllers/sendEmail.controller");
-
+const {addNewContactInfo, createConnection} = require("../controllers/sqlQueries.controller")
+const {getNewToken, validateToken} = require("../controllers/uuid.controller")
+const myConnection = createConnection()
 module.exports = (router) => {
   //Mailing
   router.post(
-    "/sendVerificateEmail",
+    "/sendinfo",
     [
       check("email").normalizeEmail().isEmail(),
       check("country").isAlpha(),
       check("name").isAlpha(),
       check("lastname").isAlpha(),
       check("address").isAlphanumeric(),
-      check("wallet").isBTCAddress(),
+      check("wallet").isAlphanumeric(),
       check("city").isAlpha(),
       check("province").isAlpha(),
       check("zipcode").isInt(),
       check("phone").isInt(),
     ],
-    sendEmail
+    async function toDB (req,res) {
+      await addNewContactInfo(req,myConnection,getNewToken())
+      sendEmail(req,res)
+    }
   );
 };
